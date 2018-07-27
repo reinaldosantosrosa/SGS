af21da13c619422284238a1bb0b237ec17f40ec6
reinaldo <rei16703@gmail.com>
2018-07-27 00:44:01 -0300

    Adicionar arquivos de projeto.

---
+++ SRC/SGS.app/Entity/Beneficiario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGS.app.Entity
+{
+    public class Beneficiario
+    {
+
+        public Beneficiario()
+        {
+
+        }
+        
+        public int BeneficiarioID { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+    }
+
+
+}
